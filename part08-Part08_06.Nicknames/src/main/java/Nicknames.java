eb9d42592c2a48aaa94e3d0e1b6f6174db028acb
Benrichie Hewson <um2341583@gmail.com>
2022-12-13 19:24:35 +1100

    creating_HashMap

--- part08-Part08_06.Nicknames/src/main/java/Nicknames.java
+++ part08-Part08_06.Nicknames/src/main/java/Nicknames.java
@@ -5,6 +5,13 @@ public class Nicknames {
 
     public static void main(String[] args) {
         // Do the operations required here!
+        HashMap<String, String> hm = new HashMap<>();
+        
+        hm.put("matthew", "matt");
+        hm.put("michael", "mix");
+        hm.put("arthur", "art");
+        
+        System.out.println(hm.get("matthew"));
     }
 
 }
