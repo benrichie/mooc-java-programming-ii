d330dd1e7f8ee4fa7564ed29a0b41afeb073775b
Benrichie Hewson <um2341583@gmail.com>
2023-01-10 18:28:00 +1100

    09_10,2,3

--- part09-Part09_10.OnlineShop/src/main/java/Warehouse.java
+++ part09-Part09_10.OnlineShop/src/main/java/Warehouse.java
@@ -10,23 +10,63 @@
  */
 import java.util.HashMap;
 import java.util.Map;
+import java.util.AbstractCollection;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Warehouse {
     
     private Map<String, Integer> Product;
-    private int stock;
+    private Map<String, Integer> stockBalance;
     
     public Warehouse(){
         this.Product = new HashMap<>();
+        this.stockBalance = new HashMap<>();
     }
     
     public void addProduct(String product, int price, int stock){
+        
         Product.put(product, price);
-        this.stock += stock;
+        stockBalance.put(product, stock);
+        
     }
     
     public int price(String product){
         
         return Product.getOrDefault(product, -99);
     }
+    
+    public int stock(String product){
+        
+        return stockBalance.getOrDefault(product, 0);
+    }
+    
+    public boolean take(String product){
+        
+        if(this.stock(product) != 0){
+            
+            stockBalance.put(product, stockBalance.get(product) - 1 );
+                    
+            return true;
+        }
+        
+        return false;
+    }
+    
+    public Set<String> products(){
+        // create  hashSet to go through 
+        // then print the lil bih
+        
+        
+        
+        Set<String> productSet = new HashSet<>();
+        
+        for(String key : Product.keySet()){
+            productSet.add(key);
+        }
+        
+        return productSet;
+    }
+
+    
 }
